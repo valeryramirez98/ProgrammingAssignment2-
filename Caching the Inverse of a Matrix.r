04622a369018e85747d1c1a40aed94f1868a98da
valeryramirez98 <42686601+valeryramirez98@users.noreply.github.com>
2018-09-08 21:13:10 -0500

    Add files via upload

---
+++ Caching the Inverse of a Matrix.r
@@ -0,0 +1,51 @@
+makeCacheMatrix <- function(x =matrix()) {
+       
+ i <- NULL
+       
+ set <- function(y) {
+               
+ x <<- y
+               
+ i <<- NULL
+      
+  }
+        
+get <- function() x       
+setsolve <- function(solve) i<<- inverse  
+getsolve<- function() i     
+  list(set = set, get = get,
+           
+  setsolve = setsolve,
+           
+  getsolve = getsolve)
+}
+
+
+cacheSolve<- function(x, ...) {
+        
+i <- x$getsolve()
+        
+if(!is.null(i)) {
+  
+              message("getting cached data")
+                return(i)
+       
+ }
+        
+data <- x$get()
+        
+i <- solve(data, ...)
+       
+x$setdata(i)
+        
+i
+}
+
+
+
+
+
+
+
+
+
